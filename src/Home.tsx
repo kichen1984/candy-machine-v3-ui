44366e5268c4fc162c6c547e15069526305beac1
Kichen <kichen1984@live.com>
2023-02-27 19:16:39 -0600

    Update Home.tsx

--- src/Home.tsx
+++ src/Home.tsx
@@ -361,7 +361,7 @@ const Home = (props: HomeProps) => {
   });
 
   const { guardLabel, guards, guardStates, prices } = useMemo(() => {
-    const guardLabel = "public";
+    const guardLabel = "Public";
     return {
       guardLabel,
       guards:
